20fe2c708161dbf125dcc077706e8a5655362839
Unknown <Calvin.lapp@dcmail.ca>
2021-11-25 15:28:21 -0500

    loading for search

--- client/components/friends/AddFriendModal.tsx
+++ client/components/friends/AddFriendModal.tsx
@@ -1,5 +1,6 @@
 import { useLazyQuery } from "@apollo/client";
 import { Transition, Dialog } from "@headlessui/react";
+import { TruckIcon } from "@heroicons/react/solid";
 import React, { Fragment } from "react";
 import {
   PendingFriendsDocument,
@@ -19,10 +20,12 @@ export const AddFriendModal: React.FC<AddFriendModalProps> = ({
   closeModal,
   isOpen,
 }) => {
-  const [searchForUser, { loading, error, data: searchData, refetch }] =
-    useLazyQuery<SearchForUserQuery>(SearchForUserDocument, {
-      fetchPolicy: "no-cache",
-    });
+  const [
+    searchForUser,
+    { loading: searchLoading, error, data: searchData, refetch },
+  ] = useLazyQuery<SearchForUserQuery>(SearchForUserDocument, {
+    fetchPolicy: "no-cache",
+  });
   const [addFriend, { loading: addFriendLoading }] = useAddFriendMutation({
     update(cache, { data }) {
       if (data?.addFriend.friend) {
@@ -41,6 +44,46 @@ export const AddFriendModal: React.FC<AddFriendModalProps> = ({
       }
     },
   });
+
+  const DisplaySearchData = () => {
+    if (searchLoading)
+      return (
+        <div className="text-viat-666 mt-4 flex justify-center space-x-2">
+          <p className="text-center italic">Beep beep, loading...</p>
+          <TruckIcon className="w-4" />
+        </div>
+      );
+    if (!searchData)
+      return (
+        <p className="italic text-viat-666 text-center mt-4">Type something</p>
+      );
+    if (searchData.searchForUser && searchData.searchForUser.length <= 0)
+      return (
+        <p className="italic text-viat-666 text-center mt-4">
+          No users match that search
+        </p>
+      );
+    return (
+      <>
+        {searchData?.searchForUser?.map((user) => {
+          return (
+            <AddFriendDetail
+              key={user.username}
+              displayName={user.displayName}
+              username={user.username}
+              disabled={addFriendLoading}
+              addFriend={async () => {
+                await addFriend({
+                  variables: { username: user.username },
+                });
+                refetch && (await refetch());
+              }}
+            />
+          );
+        })}
+      </>
+    );
+  };
 
   const validateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const text = e.target.value;
@@ -99,27 +142,12 @@ export const AddFriendModal: React.FC<AddFriendModalProps> = ({
                   <input
                     type="text"
                     placeholder="Enter a username"
-                    className="appearance-none w-full shadow-md rounded py-2 px-3 leading-tight focus:outline-none focus:ring bg-[#4a4a4a] border-[#1a1a1a] border"
+                    className="appearance-none w-full shadow-md rounded py-2 px-3 leading-tight focus:outline-none focus:ring-2 focus-visible:ring-purple-500 bg-[#4a4a4a] border-[#1a1a1a] border"
                     onChange={(e) => validateSearch(e)}
                   />
                 </div>
                 <div className="mt-2 rounded overflow-y-auto h-64 scrollbar scrollbar-thin scrollbar-thumb-viat-666 scrollbar-track-viat-medium scrollbar-thumb-rounded-md scrollbar-track-rounded-md">
-                  {searchData?.searchForUser?.map((user) => {
-                    return (
-                      <AddFriendDetail
-                        key={user.username}
-                        displayName={user.displayName}
-                        username={user.username}
-                        disabled={addFriendLoading}
-                        addFriend={async () => {
-                          await addFriend({
-                            variables: { username: user.username },
-                          });
-                          refetch && (await refetch());
-                        }}
-                      />
-                    );
-                  })}
+                  <DisplaySearchData />
                 </div>
               </div>
             </Transition.Child>
